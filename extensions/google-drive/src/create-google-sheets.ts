40f55d1da5f1dbdcf9dd4194acd00af061ab65de
Ahmad Kholid <kholid060@gmail.com>
2024-09-24 16:27:42 +0800

    feat(google-drive): add toast when creating file

--- extensions/google-drive/src/create-google-sheets.ts
+++ extensions/google-drive/src/create-google-sheets.ts
@@ -1,4 +1,8 @@
-import { CommandLaunchContext } from '@altdot/extension';
+import {
+  _extension,
+  CommandLaunchBy,
+  CommandLaunchContext,
+} from '@altdot/extension';
 import { GoogleWorkspaceConfig } from './interface/google-workspace.interface';
 import GDriveAPI from './utils/GDriveAPI';
 import fileAction from './utils/fileAction';
@@ -6,7 +10,16 @@ import { getFileLink } from './utils/helper';
 
 export default async function createGoogleSheets({
   args,
+  launchBy,
 }: CommandLaunchContext<GoogleWorkspaceConfig>) {
+  const toast =
+    launchBy !== CommandLaunchBy.WORKFLOW
+      ? _extension.ui.createToast({ title: 'Creating file', type: 'loading' })
+      : null;
+  toast?.show();
+
   const result = await GDriveAPI.createGoogleSheets(args.title);
   await fileAction(getFileLink('google-sheets', result.spreadsheetId));
+
+  toast?.hide();
 }
